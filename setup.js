5281a6d39087dc0110203867195a085c57d95534
Yashas <p.yahha@yahoo.com>
2025-12-18 21:05:21 +0530

    Delete setup.js

--- setup.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("startBtn").onclick = () => {
-  sessionStorage.setItem("role", role.value);
-  sessionStorage.setItem("experience", experience.value);
-  sessionStorage.setItem("company", company.value);
-  window.location.href = "interview.html";
-};
